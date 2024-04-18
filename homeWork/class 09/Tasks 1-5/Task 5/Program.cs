f6410c177c9c8178cef27a5c5817ee4ce9701cd1
ppsasho <businessmail5545@gmail.com>
2024-04-18 17:44:42 +0200

    - class 09 homework

---
+++ homeWork/class 09/Tasks 1-5/Task 5/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = "Exercises";
+            var filePath = @$"{folder}\calculations.txt";
+
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            else Console.WriteLine("Folder is already created");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Calculation no.{i + 1}");
+                int a = GetNumber("Enter your first number:");
+                int b = GetNumber("Enter your second number:");
+                string result = Sum(a, b);
+                Console.Clear();
+
+                Console.WriteLine(result);
+                File.AppendAllText(filePath, $"{result}\n");
+            }
+        }
+        #region Methods
+        static int GetNumber(string msg)
+        {
+            while (true)
+            {
+                if (int.TryParse(GetInput(msg), out int number)) return number;
+                else Console.WriteLine("Please make sure to enter a number");
+                continue;
+            }
+        }
+        static string GetInput(string msg)
+        {
+            while (true)
+            {
+                Console.Write(msg);
+                string input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input)) return input;
+                else Console.WriteLine("Please don't leave empty inputs.");
+                continue;
+            }
+        }
+        static string Sum(int a, int b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+        #endregion
+    }
+}
---
+++ homeWork/class 09/Tasks 1-5/Tasks 1-4/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Tasks_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var alphabetList = new List<char>()
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            };
+            var folder = "Files";
+            var filePath = @$"{folder}\names.txt";
+
+            if(!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            else Console.WriteLine("Folder is already created");
+
+            File.WriteAllText(filePath, "");
+
+            while (true)
+            {
+                var names = new List<string>();
+
+                while (true)
+                {
+                    var name = GetInput("Enter a name: ");
+                    names.Add(name);
+                    switch(GetInput("Would you like to enter a new name? (Y N):").ToUpper())
+                    {
+                        case "Y": continue;
+
+                        case "N":
+                        default : break;
+                    }
+                    break;
+                }
+
+                File.AppendAllLines(filePath, names);
+
+                using (var sr = new StreamReader(filePath))
+                {
+                    while (true)
+                    {
+                        var line = sr.ReadLine();
+                        if (string.IsNullOrEmpty(line)) break;
+
+                        Action<char> checkFileForNames = (letter) =>
+                        {
+                            if (line.ToLower().StartsWith(letter)){
+                                string newFilePath = @$"{folder}\namesStartingWith_{letter.ToString().ToUpper()}.txt";
+                                File.AppendAllText(newFilePath, $"{line}\n");
+                            }
+                        };
+                        alphabetList.ForEach(checkFileForNames);
+                    }
+                    Console.WriteLine("All entered names have been filtered!");
+                };
+
+                switch(GetInput("Would you like to try again? (Y N): ").ToUpper())
+                {
+                    case "Y": continue;
+
+                    case "N":
+                    default: break;
+                }
+                break;
+            }
+        }
+        static string GetInput(string msg)
+        {
+            while (true)
+            {
+                Console.Write(msg);
+                string input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input)) return input;
+                    else Console.WriteLine("Please don't leave empty inputs.");
+                    continue;
+            }
+        }
+    }
+}
