3ebaefe0292a28ec6c8b3e4555e1d12a6a5c3934
ppsasho <businessmail5545@gmail.com>
2024-04-27 10:13:28 +0200

    - organized TryBeingFit
    + some schoolwork

--- homeWork/TryBeingFitApp/TryBeingFit/Data/Data.cs
+++ homeWork/TryBeingFitApp/TryBeingFit/Data/Data.cs
@@ -1,4 +1,5 @@
 ﻿namespace Models
+   
 {
     public static class Data
     {
---
+++ schoolWork/class 12/Async Await/Threads/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessagesWithThreads();
+        }
+        static void SendMessagesWithThreads()
+        {
+            Console.WriteLine("Getting ready..");
+            Thread.Sleep(2000);
+            Thread myThread = new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("First msg.");
+            });
+            myThread.Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Second msg.");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Third msg.");
+            }).Start();
+            Console.WriteLine("All msgs received.");
+        }
+    }
+}
